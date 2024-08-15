c1555e5b7f8eaf19df21d0b8a5ebf0beb8921ed0
AJPiero <AJPiero@users.noreply.github.com>
2024-08-15 04:15:00 -0400

    drag

--- screens/CreateFormScreen.js
+++ screens/CreateFormScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Keyboard, Dimensions, Platform } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Keyboard, Dimensions, Platform, ScrollView, TouchableOpacity } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { Formik, FieldArray } from 'formik';
 import * as Yup from 'yup';
 import { addForm } from '../slices/formsSlice';
 import { Picker } from '@react-native-picker/picker';
+import { useDragAndDrop } from '@formkit/drag-and-drop/react';
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
@@ -49,83 +50,105 @@ export default function CreateFormScreen({ navigation }) {
         resetForm();
       }}
     >
-      {({ handleChange, handleBlur, handleSubmit, values, setFieldValue, touched, errors }) => (
-        <View style={styles.outerContainer}>
-          <View style={styles.container}>
-            <Text style={styles.label}>Name of Form:</Text>
-            <TextInput
-              style={styles.formNameInput}
-              onChangeText={handleChange('formName')}
-              onBlur={handleBlur('formName')}
-              value={values.formName}
-              onSubmitEditing={() => Keyboard.dismiss()} 
-              returnKeyType="done" 
-            />
-            {touched.formName && errors.formName ? (
-              <Text style={styles.error}>{errors.formName}</Text>
-            ) : null}
+      {({ handleChange, handleBlur, handleSubmit, values, setFieldValue, touched, errors }) => {
+        const [parentRef, fieldOrder, setFieldOrder] = useDragAndDrop(
+          values.fields.map((_, index) => index), 
+          {
+            draggable: (el) => el.id !== 'no-drag',
+            handle: '.handle', 
+          }
+        );
 
-            <FieldArray
-              name="fields"
-              render={({ push, remove }) => (
-                <>
-                  {values.fields.map((field, index) => (
-                    <View key={index} style={styles.fieldGroup}>
-                      <View style={styles.fieldContainer}>
-                        <Picker
-                          selectedValue={field.type}
-                          style={styles.smallPicker}
-                          onValueChange={(itemValue) => setFieldValue(`fields[${index}].type`, itemValue)}
-                        >
-                          <Picker.Item label="Text" value="text" />
-                          <Picker.Item label="Number" value="number" />
-                        </Picker>
-                        
-                        <TextInput
-                          style={styles.titleInput}
-                          placeholder="Title"
-                          onChangeText={(value) => setFieldValue(`fields[${index}].title`, value)}
-                          onBlur={handleBlur(`fields[${index}].title`)}
-                          value={field.title}
-                        />
-                        
-                        <TextInput
-                          style={styles.input}
-                          placeholder={field.type === 'number' ? 'Write total' : 'Write text'}
-                          onChangeText={(value) => setFieldValue(`fields[${index}].value`, value)}
-                          onBlur={handleBlur(`fields[${index}].value`)}
-                          value={field.value}
-                          onSubmitEditing={() => Keyboard.dismiss()} 
-                          returnKeyType="done" 
-                        />
-                        
-                        <Button title="Remove" onPress={() => remove(index)} style={styles.button} />
-                      </View>
-                      {/* Error messages */}
-                      {touched.fields && touched.fields[index] && errors.fields && errors.fields[index] && (
-                        <View>
-                          {errors.fields[index].type && <Text style={styles.error}>{errors.fields[index].type}</Text>}
-                          {errors.fields[index].title && <Text style={styles.error}>{errors.fields[index].title}</Text>}
-                          {errors.fields[index].value && <Text style={styles.error}>{errors.fields[index].value}</Text>}
+        return (
+          <View style={styles.outerContainer}>
+            <ScrollView style={styles.container}>
+              <Text style={styles.label}>Name of Form:</Text>
+              <TextInput
+                style={styles.formNameInput}
+                onChangeText={handleChange('formName')}
+                onBlur={handleBlur('formName')}
+                value={values.formName}
+                onSubmitEditing={() => Keyboard.dismiss()} 
+                returnKeyType="done" 
+              />
+              {touched.formName && errors.formName ? (
+                <Text style={styles.error}>{errors.formName}</Text>
+              ) : null}
+
+              <FieldArray
+                name="fields"
+                render={({ push, remove }) => (
+                  <>
+                    <View ref={parentRef}>
+                      {fieldOrder.map((index) => (
+                        <View key={index} style={styles.fieldGroup}>
+                          <View style={styles.fieldContainer}>
+                            <TouchableOpacity style={styles.handle} activeOpacity={0.7}>
+                              <Text style={styles.handleText}>≡</Text>
+                            </TouchableOpacity>
+                            <Picker
+                              selectedValue={values.fields[index].type}
+                              style={styles.smallPicker}
+                              onValueChange={(itemValue) => setFieldValue(`fields[${index}].type`, itemValue)}
+                            >
+                              <Picker.Item label="Text" value="text" />
+                              <Picker.Item label="Number" value="number" />
+                            </Picker>
+
+                            <TextInput
+                              style={styles.titleInput}
+                              placeholder="Title"
+                              onChangeText={(value) => setFieldValue(`fields[${index}].title`, value)}
+                              onBlur={handleBlur(`fields[${index}].title`)}
+                              value={values.fields[index].title}
+                            />
+
+                            <TextInput
+                              style={styles.input}
+                              placeholder={values.fields[index].type === 'number' ? 'Write total' : 'Write text'}
+                              onChangeText={(value) => setFieldValue(`fields[${index}].value`, value)}
+                              onBlur={handleBlur(`fields[${index}].value`)}
+                              value={values.fields[index].value}
+                              onSubmitEditing={() => Keyboard.dismiss()} 
+                              returnKeyType="done" 
+                            />
+
+                            <Button title="Remove" onPress={() => {
+                              remove(index);
+                              setFieldOrder(fieldOrder.filter(i => i !== index));
+                            }} style={styles.button} />
+                          </View>
+                          {/* Error messages */}
+                          {touched.fields && touched.fields[index] && errors.fields && errors.fields[index] && (
+                            <View>
+                              {errors.fields[index].type && <Text style={styles.error}>{errors.fields[index].type}</Text>}
+                              {errors.fields[index].title && <Text style={styles.error}>{errors.fields[index].title}</Text>}
+                              {errors.fields[index].value && <Text style={styles.error}>{errors.fields[index].value}</Text>}
+                            </View>
+                          )}
                         </View>
-                      )}
+                      ))}
                     </View>
-                  ))}
 
-                  <Button
-                    title="+ Add New Field"
-                    onPress={() => push({ title: '', type: 'text', value: '' })}
-                  />
-                </>
-              )}
-            />
+                    <Button
+                      title="+ Add New Field"
+                      onPress={() => {
+                        const newField = { title: '', type: 'text', value: '' };
+                        push(newField);
+                        setFieldOrder([...fieldOrder, values.fields.length]); 
+                      }}
+                    />
+                  </>
+                )}
+              />
 
-            <View style={styles.spacer} />
+              <View style={styles.spacer} />
 
-            <Button title="Submit" onPress={handleSubmit} />
+              <Button title="Submit" onPress={handleSubmit} />
+            </ScrollView>
           </View>
-        </View>
-      )}
+        );
+      }}
     </Formik>
   );
 }
@@ -182,6 +205,18 @@ const styles = StyleSheet.create({
   },
   button: {
     flex: 0.2,
+  },
+  handle: {
+    width: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+    backgroundColor: 'transparent',
+    borderRadius: 4,
+  },
+  handleText: {
+    fontSize: 20,
+    color: '#333',
   },
   error: {
     fontSize: 12,
